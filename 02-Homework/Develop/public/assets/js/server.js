5e8cc0ae2da5fab86c9d53d1bac10d6f46c2aef4
AudioDen <chicagosoundman@gmail.com>
2021-04-19 10:20:48 -0500

    routes

--- 02-Homework/Develop/public/assets/js/server.js
+++ 02-Homework/Develop/public/assets/js/server.js
@@ -1,7 +1,7 @@
 // DEPENDECIES
 const express = require("express");
-
 const app = express();
+
 const PORT =  process.env.PORT || 3000
 
 // data parsing 
@@ -11,8 +11,8 @@ app.use(express.static("public"));
 
 //ROUTING FOR HTML AND API
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+//require("./routes/apiRoutes")(app);
+require("/routes/htmlRoutes")(app);
 
 // PORT ASSIGNMENT  LISTNER
 app.listen(PORT, () => console.log(`we are listening on PORT: ${PORT}`))
